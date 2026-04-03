8073afa2f0b77b3d3ca452f5cea40cef263da432
elettro <dpalermo@elettro.com>
2026-04-03 11:15:08 -0400

    Fix MailerLite embed submit flow and load universal snippet

--- js/main.js
+++ js/main.js
@@ -281,6 +281,17 @@
  window.addEventListener('orientationchange', scheduleUpdate);
 })();
 
+
+(() => {
+ const existingUniversalScript = document.querySelector('script[data-ml-universal="true"]');
+ if (existingUniversalScript || typeof window.ml === 'function') return;
+
+ const universalScript = document.createElement('script');
+ universalScript.setAttribute('data-ml-universal', 'true');
+ universalScript.text = "(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[]).push(arguments);},l=d.createElement(e),l.async=1,l.src=u,n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');ml('account', '2240972');";
+ document.head.appendChild(universalScript);
+})();
+
 (() => {
  const footerAlreadyPresent = document.querySelector('.site-global-footer');
  if (footerAlreadyPresent) return;
